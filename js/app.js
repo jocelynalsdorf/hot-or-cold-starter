ec625b9269de820fb11e99ea3c18767e104b9fdf
jocelynalsdorf <jfrodsla@gmail.com>
2014-11-22 16:24:58 +0700

    set up button with click event and generated random number

--- js/app.js
+++ js/app.js
@@ -12,6 +12,27 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
-});
+
+/*---generate a random number between 1-100 too start the game---*/
+
+	
+
+	$(".new").on("click",function() {
+	function getNum() {
+    return Math.floor(Math.random() * (100)) + 1;
+	}
+	var randomNum = getNum();
+	alert(randomNum);
+
+	});
+
+
+
+
+
+
+/*--- Hide information modal box ---*/
+
+	});
 
 
