e0665ed8a678f943b0b4802825308f78f66a86b1
jocelynalsdorf <jfrodsla@gmail.com>
2014-11-23 14:46:31 +0700

    created function to compare random number to the user number and give input. made the new gameclick event handler surround all functions so that variables were available for calculation

--- js/app.js
+++ js/app.js
@@ -26,37 +26,83 @@ $(document).ready(function(){
 	var randomNum = newNum();
 	alert(randomNum);
 
-	});
+	
 
 
 
 /*--- take input from user and append to list ---*/
 
 
-var count = 0;
+	var count = 0;
 
-$('form').submit(function(event) {
-     event.preventDefault();
-	   var textValue = $("#userGuess").val();
-	   var x = textValue; 
+
+
+	$('form').submit(function(event) {
+    event.preventDefault();
+	var textValue = $("#userGuess").val();
+	var x = textValue; 
+
  	    	 
- 	   function countTries() {
-            document.getElementById("count").innerHTML = ++count;
-           
-            } 
-     if (textValue ==="" || textValue <= 0 || textValue > 100 || (isNaN(x))) {
-       alert("enter a number between 1-100");
-     }
-     else {
-     	
-     	$("#guessList").append("<li class=\"guess-items\"><span>" + textValue + "</span></li>");
-		return countTries();	
-			}
+ 	function countTries() {
+    document.getElementById("count").innerHTML = ++count;
+    } 
+
+    
+    
+    if (textValue ==="" || textValue <= 0 || textValue > 100 || (isNaN(x))) {
+    alert("enter a number between 1-100");
+    }
+
+    else {
+    $("#guessList").append("<li class=\"guess-items\"><span>" + textValue + "</span></li>");
+	return countTries();	
+	
+	}
               
 	});
+	
+/*----------give user input on guess-------*/
 
 
 
+
+$(".button").on("click", function() {
+	var textValue = $("#userGuess").val();
+var p = Math.abs(textValue - randomNum);
+function between(p, min, max) {
+    return p >= min && p <= max;	
+
+	  }
+
+if (p === 0) {
+     alert("You win!!");
+
+ } 
+else if (between(p, 50 , 100))  {
+     alert("ice cold");
+
+ }
+    
+else if (between(p, 30 , 49))  {
+	  alert("cool");
+
+ }
+    
+else if (between(p, 10 , 30))  {
+     alert("warm");
+
+ } 
+  
+else  if (between(p, 1 , 29)) {
+     alert("hot");
+ }
+
+ else {
+ 
+ 
+}
+});
+});
 	});
 
 
